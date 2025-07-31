d903062f0c24f0acce7335f650c93ba152184817
Your Name <you@example.com>
2025-08-01 04:57:57 +0500

    Initial commit

---
+++ src/MacContainer.jsx
@@ -0,0 +1,12 @@
+import { Stars, useGLTF } from '@react-three/drei'
+import React from 'react'
+
+const MacContainer = () => {
+  let model = useGLTF("./mac.glb")
+  return <group position={[0,-10,20]}>
+<primitive object={model.scene} />
+  </group> 
+};
+
+export default MacContainer
+
