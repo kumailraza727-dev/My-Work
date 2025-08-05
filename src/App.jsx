066e6e0df22af03292379efe31a6fa36c6e6eb7f
Your Name <you@example.com>
2025-08-05 21:57:23 +0500

    Describe what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -57,9 +57,9 @@ const App = () => {
       {/* Hero Section */}
       <div className="hero-section relative flex items-center justify-center">
         <div className='items-center w-[100%] flex flex-col gap-[20px]'>
-          <h1 className='text-[64px] font-semibold text-white flex leading-[74px] text-center'>
+          <h3 className='text-[64px] font-semibold text-white flex leading-[74px] text-center'>
             Your Trusted Telecom <br /> Consultancy
-          </h1>
+          </h3>
           <p className='text-white'>Expert guidance for all your telecom needs today.</p>
           <a href="#check1" className='text-center border border-white rounded-[50px] p-[10px] text-[20px] font-medium w-[12%] text-white cursor-pointer'>
             Get Started
@@ -67,15 +67,15 @@ const App = () => {
 
           <div className="hero-sections flex w-[100%] justify-around pt-[50px]">
             <div className='sec-1 flex flex-col gap-y-[10px] p-[25px] bg-[#FFFFFF1F] text-center w-[30%]'>
-              <h1 className='flex justify-center text-white font-semibold'>Tailored Solutions</h1>
+              <h2 className='flex justify-center text-white font-semibold'>Tailored Solutions</h2>
               <p className='text-white'>Customized telecom packages for your requirements.</p>
             </div>
             <div className='sec-2 flex flex-col gap-y-[10px] p-[25px] bg-[#FFFFFF1F] text-center w-[30%] '>
-              <h1 className='flex justify-center text-white font-semibold'>Billing Support</h1>
+              <h2 className='flex justify-center text-white font-semibold'>Billing Support</h2>
               <p className='text-white'>Resolve network issues with our expert support.</p>
             </div>
             <div className='sec-3 flex flex-col gap-y-[10px] p-[25px] bg-[#FFFFFF1F] text-center w-[30%] '>
-              <h1 className='flex justify-center text-white font-semibold'>Telecom Audits</h1>
+              <h3 className='flex justify-center text-white font-semibold'>Telecom Audits</h3>
               <p className='text-white'>Affordable financing options for your devices.</p>
             </div>
           </div>
@@ -85,7 +85,7 @@ const App = () => {
       {/* First Section */}
       <div id='check1' className="first-sec flex justify-center py-[30px] max-w-[1400px] w-[100%] content-end">
         <div className='left gap-[60px] flex flex-col w-[50%] content-start justify-center'>
-          <h1 className='text-[48px] font-semibold flex leading-[58px] '>Your Trusted <br />Telecom <br /> Consultancy</h1>
+          <h3 className='text-[48px] font-semibold flex leading-[58px] '>Your Trusted <br />Telecom <br /> Consultancy</h3>
           <p>Expert guidance for all your telecom needs today.</p>
           <a href="" className='text-center border border-black rounded-[50px] p-[10px] text-[20px] font-medium w-[20%] cursor-pointer'>
             Get Started
@@ -102,7 +102,7 @@ const App = () => {
           <img className='w-[90%] rounded-[25px]' src="https://rogarcomm.com/wp-content/uploads/2025/04/RKXEW525TJA3EFJ5I2MDW1MKVAXNCH41VEY4RWQJIZLI3DVP.webp" alt="" />
         </div>
         <div className='right gap-[60px] flex flex-col w-[50%] content-start justify-center pl-[85px]'>
-          <h1 className='text-[48px] font-semibold flex leading-[58px] '>Who We Are</h1>
+          <h3 className='text-[48px] font-semibold flex leading-[58px] '>Who We Are</h3>
           <p>At Rogar Communication, we empower individuals and businesses to navigate telecom choices with ease, ensuring tailored solutions for every need.</p>
         </div>
       </div>
@@ -110,23 +110,23 @@ const App = () => {
       {/* Third Section */}
       <div className="third-sec bg-[#E6F7FF] max-w-[1400px] w-[100%] flex pt-[50px] flex-col">
         <div className='w-[100%] flex flex-col items-center gap-[20px] '>
-          <h1 className='text-[48px] font-semibold flex leading-[58px] '>Our Services</h1>
+          <h3 className='text-[48px] font-semibold flex leading-[58px] '>Our Services</h3>
           <p>At Rogar Communication, we empower individuals and businesses to navigate telecom choices with ease, ensuring tailored solutions for every need.</p>
         </div>
         <div className="inner-sections flex w-[100%] justify-around pt-[50px]">
           <div className='sec-1 flex flex-col gap-y-[10px] p-[25px] bg-[#FFFFFF1F] text-center w-[30%]'>
             <img className='rounded-[25px]' src="https://rogarcomm.com/wp-content/uploads/2025/04/photo-1567872822303-59ec11c24dea.webp" alt="" />
-            <h1 className='flex text-black font-semibold'>Network Issue Resolution</h1>
+            <h3 className='flex text-black font-semibold'>Network Issue Resolution</h3>
             <p className='text-[#847A8B] text-left'>We help resolve network issues quickly and efficiently, ensuring seamless connectivity for your services.</p>
           </div>
           <div className='sec-2 flex flex-col gap-y-[10px] p-[25px] bg-[#FFFFFF1F] text-center w-[30%] '>
             <img className='rounded-[25px]' src="https://rogarcomm.com/wp-content/uploads/2025/04/photo-1695136228645-afa4dba2c7de.webp" alt="" />
-            <h1 className='flex text-black font-semibold'>Package Selection</h1>
+            <h3 className='flex text-black font-semibold'>Package Selection</h3>
             <p className='text-[#847A8B] text-left'>Assistance in choosing the right telecom packages that fit your specific needs and budget.</p>
           </div>
           <div className='sec-3 flex flex-col gap-y-[10px] p-[25px] bg-[#FFFFFF1F] text-center w-[30%] '>
             <img className='rounded-[25px]' src="https://rogarcomm.com/wp-content/uploads/2025/04/photo-1495156613386-c20b942f356d.webp" alt="" />
-            <h1 className='flex text-black font-semibold'>Telecom Audits</h1>
+            <h3 className='flex text-black font-semibold'>Telecom Audits</h3>
             <p className='text-[#847A8B] text-left'>Comprehensive telecom audits for identifying savings and optimizing your current telecom services and expenses.</p>
           </div>
         </div>
@@ -149,7 +149,7 @@ const App = () => {
         <div className='w-[100%] flex flex-row'>
           <div className='w-[33.3%] flex flex-col items-center gap-[20px] text-left '>
             <div className='gap-[20px] flex flex-col'>
-              <h1 className='text-white font-semibold text-[23px] '>Services</h1>
+              <h3 className='text-white font-semibold text-[23px] '>Services</h3>
               <p className='text-white text-[18px] text-left'>Expert telecom consultancy for informed decision-making.</p>
             </div>
           </div>
